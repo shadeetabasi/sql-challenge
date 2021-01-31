d14c59f56a115415683ca91e4b2374a659518d70
Shadee Tabasi <shadeetabasi@gmail.com>
2021-01-30 20:31:26 -0500

    Make table names lowercase

--- EmployeeSQL/create_employees_db.sql
+++ EmployeeSQL/create_employees_db.sql
@@ -3,31 +3,31 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Departments" (
+CREATE TABLE "departments" (
     "dept_no" VARCHAR(255)   NOT NULL,
     "dept_name" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
+    CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
-CREATE TABLE "Department_Employees" (
+CREATE TABLE "department_dmployees" (
     "emp_no" INT   NOT NULL,
     "dept_no" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Department_Employees" PRIMARY KEY (
+    CONSTRAINT "pk_department_employees" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
-CREATE TABLE "Dept_manager" (
+CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(255)   NOT NULL,
     "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_no","emp_no"
      )
 );
 
-CREATE TABLE "Employees" (
+CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR(255)   NOT NULL,
     "birth_date" DATE   NOT NULL,
@@ -35,42 +35,42 @@ CREATE TABLE "Employees" (
     "last_name" VARCHAR(255)   NOT NULL,
     "sex" VARCHAR(1)   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
+    CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Salaries" (
+CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Titles" (
+CREATE TABLE "titles" (
     "title_id" VARCHAR(255)   NOT NULL,
     "title" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
+    CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
-ALTER TABLE "Department_Employees" ADD CONSTRAINT "fk_Department_Employees_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "department_employees" ADD CONSTRAINT "fk_department_employees_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
 
-ALTER TABLE "Department_Employees" ADD CONSTRAINT "fk_Department_Employees_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "department_employees" ADD CONSTRAINT "fk_department_employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "Titles" ("title_id");
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
--- EmployeeSQL/load_employees_db.sql
+++ EmployeeSQL/load_employees_db.sql
@@ -1,34 +1,34 @@
-COPY public."Titles"(title_id,title
+COPY public."titles"(title_id,title
 )
 FROM '/Users/shadeetabasi/code/sql-challenge/EmployeeSQL/titles.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY public."Salaries"(emp_no,salary
+COPY public."salaries"(emp_no,salary
 )
 FROM '/Users/shadeetabasi/code/sql-challenge/EmployeeSQL/salaries.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY public."Employees"(emp_no,emp_title_id,birth_date,first_name,last_name,sex,hire_date
+COPY public."employees"(emp_no,emp_title_id,birth_date,first_name,last_name,sex,hire_date
 )
 FROM '/Users/shadeetabasi/code/sql-challenge/EmployeeSQL/employees.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY public."Departments"(dept_no,dept_name
+COPY public."departments"(dept_no,dept_name
 )
 FROM '/Users/shadeetabasi/code/sql-challenge/EmployeeSQL/departments.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY public."Department_Employees"(emp_no,dept_no
+COPY public."department_employees"(emp_no,dept_no
 )
 FROM '/Users/shadeetabasi/code/sql-challenge/EmployeeSQL/dept_emp.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY public."Dept_manager"(dept_no,emp_no
+COPY public."dept_manager"(dept_no,emp_no
 )
 FROM '/Users/shadeetabasi/code/sql-challenge/EmployeeSQL/dept_manager.csv'
 DELIMITER ','
