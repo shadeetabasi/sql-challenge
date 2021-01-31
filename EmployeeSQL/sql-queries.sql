bd550d1405aad2c058e5fa00d21119ad83488828
Shadee Tabasi <shadeetabasi@gmail.com>
2021-01-31 10:18:40 -0500

    sql queries

---
+++ EmployeeSQL/sql-queries.sql
@@ -0,0 +1,39 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, last_name, first_name, sex
+FROM employees
+LEFT JOIN salaries
+ON employees.emp_no = salaries.emp_no
+-- ORDER BY emp_no;
+
+-- List employees who were hired in 1986.
+SELECT emp_no, last_name, first_name, hire_date
+FROM public.employees
+where hire_date < '1987-01-01' and hire_date > '1985-12-31'
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, 
+employees.first_name 
+FROM dept_manager
+INNER JOIN departments ON
+dept_manager.dept_no = departments.dept_no
+INNER JOIN employees ON
+dept_manager.emp_no = employees.emp_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name
+FROM employees
+INNER JOIN dept_manager 
+ON employees.emp_no = dept_manager.emp_no
+INNER JOIN departments 
+ON departments.dept_no = dept_manager.dept_no
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
